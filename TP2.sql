24979d10822d1ad48449b6ec256c695f08ec4433
BERTON Tristan <b14019471@tempo21-2.luminy.univ-amu.fr>
2015-09-22 17:28:33 +0200

    C1 Validé avec tests C2 rédigé sans tests

--- TP2.sql
+++ TP2.sql
@@ -119,8 +119,8 @@ CREATE TABLE Horaires (
   /*
     Pour tout I1, I1 € Inscription
     Si I1[NumEtu, NumEpr] == I2[NumEtu, NumEpr]
-    Alors NOT OVERLAPS (Horaires[ I1[NumEpr] ].DateHeureDebut, idem + Epreuves[ I1[NumEpr] ].DureeEpr,
-            Horaires[ I2[NumEpr] ].DateHeureDebut, idem + Epreuves[ I2[NumEpr] ].DureeEpr)
+    Alors NOT OVERLAPS (Horaires[ I1[NumEpr] ].DateHeureDebut, Epreuves[ I1[NumEpr] ].DureeEpr,
+            Horaires[ I2[NumEpr] ].DateHeureDebut, Epreuves[ I2[NumEpr] ].DureeEpr)
 	*/
 	/*
                   Insert	Delete	Update
@@ -192,48 +192,170 @@ CREATE TABLE Horaires (
     END;
     /
   
-/* Jeu de tests */
-  /* Un étudiant inscrit à deux épreuves qui ne se chevauchent pas */
+/* Jeu de tests C1 */
+  /* C1_Modif_Epr Un étudiant inscrit à deux épreuves qui ne se chevauchent pas */
   INSERT INTO Inscriptions (NumEtu, NumEpr) VALUES (1, 1);
   INSERT INTO Inscriptions VALUES (1, 2);
-  INSERT INTO Horaires VALUES (1, '01/01/2015 00:00:00');
+  INSERT INTO Horaires (NumEpr, DateHeureDebut) VALUES (1, '01/01/2015 00:00:00');
   INSERT INTO Horaires VALUES (2, '02/01/2015 00:00:00');
-  /* Update on Epreuves: Modification de la durée de l'épreuve 1 pour créer un chevauchement */
+  /* Erreur Update on Epreuves: Modification de la durée de l'épreuve 1 pour créer un chevauchement */
   UPDATE Epreuves SET DureeEpr = '+01 10:00:00' /* Erreur -20000: Viol C1_Modif_Epr */
     WHERE Epreuves.NumEpr = 1
-  
-  DELETE FROM Inscriptions;
-  DELETE FROM Horaires;
-  
-  /* Deux épreuves qui se chevauchent, une qui ne chevauche pas */
+  /* Pas d'erreur Update on Epreuves: Modification de la durée de l'épreuve 1 sans créer un chevauchement */
+  UPDATE Epreuves SET DureeEpr = '+00 10:00:00' /* Pas d'erreur */
+    WHERE Epreuves.NumEpr = 1
+    
+    /* Reset */ 
+    DELETE FROM Inscriptions;
+    DELETE FROM Horaires;
+    UPDATE Epreuves SET DureeEpr = '+00 01:00:00'
+      WHERE Epreuves.NumEpr = 1
+    
+  /* C1_Modif_Ins Deux épreuves qui se chevauchent, deux qui ne chevauchent pas */
   INSERT INTO Horaires (NumEpr, DateHeureDebut) VALUES (1, '01/01/2015 00:00:00');
-  INSERT INTO Horaires VALUES (3, '01/01/2015 00:30:00');
+  INSERT INTO Horaires (NumEpr, DateHeureDebut) VALUES (3, '01/01/2015 00:30:00');
   INSERT INTO Horaires Values (2, '02/01/2015 00:00:00');
-  /* Insert on Inscriptions: Insertion d'une inscription d'un même étudiant sur les deux épreuves qui se chevauchent */
+  INSERT INTO Horaires Values (4, '05/01/2015 00:00:00');
+  /* Erreur Insert on Inscriptions: Insertion d'une inscription d'un même étudiant sur les deux épreuves qui se chevauchent */
   INSERT INTO Inscriptions (NumEtu, NumEpr) VALUES (1, 1);
   INSERT INTO Inscriptions VALUES (1, 3); /* Erreur -20001: Viol C1_Modif_Ins */
-  DELETE FROM Inscriptions;
-  /* Update on Inscriptions: Inscription d'un étudiant sur deux épreuves qui ne se chevauchent pas, puis modification de cette inscription */
-  INSERT INTO Inscriptions VALUES (1, 1);
-  INSERT INTO Inscriptions VALUES (1, 2);
+  /* Pas d'erreur Insert on Inscriptions: Inscription d'un étudiant sur deux épreuves qui ne se chevauchent pas */
+  INSERT INTO Inscriptions (NumEtu, NumEpr) VALUES (1, 2);
+  /* Erreur Update on Inscriptions: Modification de cette inscription sur une épreuve qui chevauche */
   UPDATE Inscriptions SET NumEpr = 3 /* Erreur -20001: Viol C1_Modif_Ins */
     WHERE
       Inscriptions.NumEtu = 1 AND
       Inscriptions.NumEpr = 2;
+  /* Pas d'erreur Update on Inscriptions: Modification de cette inscription sur une épreuve qui ne chevauche pas */
+  UPDATE Inscriptions SET NumEpr = 4 /* Pas d'erreur */
+    WHERE
+      Inscriptions.NumEtu = 1 AND
+      Inscriptions.NumEpr = 2;
+
+    /* Reset */
+    DELETE FROM Inscriptions;
+    DELETE FROM Horaires;
   
-  DELETE FROM Inscriptions;
-  DELETE FROM Horaires;
-  
-  /* Un étudiant inscrit à deux épreuves */
+  /* C1_Modif_Hor Un étudiant inscrit à deux épreuves non programmées */
   INSERT INTO Inscriptions (NumEtu, NumEpr) VALUES (1, 1);
   INSERT INTO Inscriptions VALUES (1, 2);
-  /* Insert on Horaires: Programmation des épreuves en chevauchement */
+  /* Erreur Insert on Horaires: Programmation des épreuves en chevauchement */
   INSERT INTO Horaires (NumEpr, DateHeureDebut) VALUES (1, '01/01/2015 00:00:00');
   INSERT INTO Horaires VALUES (2, '01/01/2015 00:30:00'); /* Erreur -20002: Viol C1_Modif_Hor */
-  DELETE FROM Horaires;
-  /* Update on Horaires: Les deux épreuves ne se chevauchaient pas, elles le font désormais */
-  INSERT INTO Horaires VALUES (1, '01/01/2015 00:00:00');
-  INSERT INTO Horaires VALUES (2, '02/01/2015 00:00:00');
+  /* Pas Erreur Insert on Horaires: Programmation des épreuves sans chevauchement */
+  INSERT INTO Horaires (NumEpr, DateHeureDebut) VALUES (2, '05/01/2015 00:00:00'); /* Pas d'erreur */  
+  /* Erreur Update on Horaires: Les deux épreuves ne se chevauchaient pas, elles le font désormais */
   UPDATE Horaires SET DateHeureDebut = '01/01/2015 00:00:00' /* Erreur -20002: Viol C1_Modif_Hor */
-    WHERE Horaires.NumEpr = 2 AND
-    Horaires.DateHeureDebut = '02/01/2015 00:00:00'
+    WHERE Horaires.NumEpr = 2
+  /* Pas d'erreur Update on Horaires: Les deux épreuves ne se chevauchent toujours pas */
+  UPDATE Horaires SET DateHeureDebut = '10/01/2015 00:00:00' /* Pas d'erreur */
+    WHERE Horaires.NumEpr = 2
+    
+    /* Reset */
+    DELETE FROM Inscriptions;
+    DELETE FROM Horaires;
+    
+    
+    
+/* C2 : Deux épreuves qui se chevauchent et se déroulent dans la même salle commencent au même moment */
+  /*
+    Pour tout E1, E2 € Epreuves
+    Si Occupations[E1].NumSal == Occupations[E2].NumSal
+    Et si
+      (Horaires[E1].DateHeureDebut, Epreuves[E1].DureeEpr)
+      Overlaps
+      (Horaires[E2].DateHeureDebut, Epreuves[E2].DureeEpr)
+    Alors Horaires[E1].DateHeureDebut == Horaires[E2].DateHeureDebut
+  */
+  /*
+                  Insert  Delete  Update
+      Epreuves    -       -       Imm
+      Horaires    -       -       Diff
+      Occupations Imm     -       Diff
+  */
+  
+  /* Sur Epreuves */
+  CREATE OR REPLACE TRIGGER C2_Modif_Epr
+    AFTER UPDATE OF DureeEpr ON Epreuves 
+		DECLARE N INTEGER;
+		BEGIN
+			SELECT 1 INTO N FROM Horaires H1, Horaires H2, Epreuves E1, Epreuves E2, Occupations O1, Occupations O2
+      WHERE
+        O1.NumSal = O2.NumSal AND
+        O1.NumEpr > O2.NumEpr AND
+        O1.NumEpr = H1.NumEpr AND
+        O1.NumEpr = E1.NumEpr AND
+        O2.NumEpr = H2.NumEpr AND
+        O2.NumEpr = E2.NumEpr AND
+        (H1.DateHeureDebut, E1.DureeEpr) OVERLAPS (H2.DateHeureDebut, E2.DureeEpr);
+        RAISE TOO_MANY_ROWS;
+      EXCEPTION
+        WHEN NO_DATA_FOUND THEN NULL;
+        WHEN TOO_MANY_ROWS THEN raise_application_error(-20010, 'Viol C2_Modif_Epr');
+    END;
+    /
+
+  /* Sur Horaires */
+  CREATE OR REPLACE TRIGGER C2_Modif_Hor
+    AFTER UPDATE OF NumEpr, DateHeureDebut ON Horaires 
+		DECLARE N INTEGER;
+		BEGIN
+			SELECT 1 INTO N FROM Horaires H1, Horaires H2, Epreuves E1, Epreuves E2, Occupations O1, Occupations O2
+      WHERE
+        O1.NumSal = O2.NumSal AND
+        O1.NumEpr > O2.NumEpr AND
+        O1.NumEpr = H1.NumEpr AND
+        O1.NumEpr = E1.NumEpr AND
+        O2.NumEpr = H2.NumEpr AND
+        O2.NumEpr = E2.NumEpr AND
+        (H1.DateHeureDebut, E1.DureeEpr) OVERLAPS (H1.DateHeureDebut, E1.DureeEpr);
+        RAISE TOO_MANY_ROWS;
+      EXCEPTION
+        WHEN NO_DATA_FOUND THEN NULL;
+        WHEN TOO_MANY_ROWS THEN raise_application_error(-20010, 'Viol C2_Modif_Hor');
+    END;
+    /
+  
+  /* Sur Occupations */
+  CREATE OR REPLACE TRIGGER C2_Modif_Occ
+    AFTER UPDATE OF NumSal, NumEpr ON Occupations 
+		DECLARE N INTEGER;
+		BEGIN
+			SELECT 1 INTO N FROM Horaires H1, Horaires H2, Epreuves E1, Epreuves E2, Occupations O1, Occupations O2
+      WHERE
+        O1.NumSal = O2.NumSal AND
+        O1.NumEpr > O2.NumEpr AND
+        O1.NumEpr = H1.NumEpr AND
+        O1.NumEpr = E1.NumEpr AND
+        O2.NumEpr = H2.NumEpr AND
+        O2.NumEpr = E2.NumEpr AND
+        (H1.DateHeureDebut, E1.DureeEpr) OVERLAPS (H1.DateHeureDebut, E1.DureeEpr);
+        RAISE TOO_MANY_ROWS;
+      EXCEPTION
+        WHEN NO_DATA_FOUND THEN NULL;
+        WHEN TOO_MANY_ROWS THEN raise_application_error(-20010, 'Viol C2_Modif_Occ');
+    END;
+    /
+    
+/* Jeu de tests C2 */
+  /* C2_Modif_Epr Deux épreuves dans la même salle ne se chevauchent pas */
+  INSERT INTO Horaires (NumEpr, DateHeureDebut) VALUES (1, '01/01/2015 00:00:00');
+  INSERT INTO Horaires VALUES (2, '02/01/2015 00:00:00');
+  INSERT INTO Occupations (NumSal, NumEpr, NbPlacesOcc) VALUES (1, 1, 0);
+  INSERT INTO Occupations VALUES (1, 2, 0);
+  /* Erreur Update On Epreuves: Modification de la durée de l'épreuve 1 pour créer un chevauchement */
+  UPDATE Epreuves SET DureeEpr = '+15 00:00:00' /* Erreur -20010 C2_Modif_Epr */
+    WHERE Epreuves.NumEpr = 1
+  /* Pas d'erreur Update On Epreuves: Modification de la durée de l'épreuve 1 sans créer un chevauchement */
+  UPDATE Epreuves SET DureeEpr = '+00 00:00:01' /* Pas d'erreur */
+    WHERE Epreuves.NumEpr = 1
+    
+  /* Reset */
+  DELETE FROM Horaires;
+  DELETE FROM Occupations;
+  UPDATE Epreuves SET DureeEpr = '+00 01:00:00'
+    WHERE Epreuves.NumEpr = 1
+    
+  /* C2_Modif_Hor */
+  
+  /* C2_Modif_Occ */
